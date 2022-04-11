51ff913d63d396dcb0153b2bd893f47adbf41c5e
Bekzod <bekrov21@gmail.com>
2022-04-11 16:18:04 +0500

    first setup

---
+++ script.js
@@ -0,0 +1,36 @@
+let hour = document.querySelector('.hour'),
+    minute = document.querySelector('.minute'),
+    seconds = document.querySelector('.second');
+
+// const today = new Date();
+// console.log(today);
+
+
+function getCurrentHour() {
+    let today = new Date();
+    let currentHour = today.getHours();
+    let currentMinute = today.getMinutes();
+    let currentSecond = today.getSeconds();
+
+    function getZero(num){
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num
+        }
+    }
+
+    hour.innerHTML = getZero(currentHour);
+    minute.innerHTML = getZero(currentMinute);
+    seconds.innerHTML = getZero(currentSecond);
+}
+getCurrentHour();
+
+let dateToday = new Date().getDate(),
+    monthToday = new Date().getMonth(),
+    yearToday = new Date().getFullYear();
+
+
+let currentDate = document.querySelector('.today-date').innerHTML = `${dateToday}.${monthToday}.${yearToday}`;;
+
+setInterval(getCurrentHour, 1000);
